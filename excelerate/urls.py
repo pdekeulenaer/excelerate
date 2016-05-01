32f6d30388758cd42def1ca2499c3ce6e3b795da
pdekeulenaer <playstation22>
2016-05-01 14:01:49 -0700

    New custom css

--- excelerate/urls.py
+++ excelerate/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root', settings.STATIC_ROOT}),
 ]
